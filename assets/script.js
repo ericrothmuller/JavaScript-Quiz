5a6ec68fad1f1ee29f80a369fb83967ba56f8e3d
ericrothmuller <eric@skymarketingco.com>
2022-06-26 23:31:11 -0700

    Added game questions as array objects

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,36 @@
+// Game Questions
+
+var questions = [
+    {
+        question: "What is an array?",
+        a: "A sequence of one or more characters that may consist of letters, numbers, or symbols.",
+        b: "A technique used to style certain text within the DOM.",
+        c: "A special variable which can hold more than one value. It is like a list.",
+        d: "A special way to call to the localhost in order to save data.",
+        answer: "C"
+    },
+    {
+        question: "What is global variables?",
+        a: "A declared variable that is limited to the scope of a block statement.",
+        b: "A variable that has no scope and is available throughout the entire length of code.",
+        c: "A variable that is block-scoped and can't be changed through reassignment.",
+        d: "It's when you have a function within another function.",
+        answer: "B"
+    },
+    {
+        question: "What is the use of isNaN function?",
+        a: "It is used to determine what operating system a user is using.",
+        b: "It always returns no value or no object.",
+        c: "Produces a negative infinity number by dividing a negative number by zero.",
+        d: "It returns true if the argument is not a number, otherwise it is false.",
+        answer: "D"
+    }
+
+
+]
+
+
+
 // Button that activates the timer begins here.
 
 document.getElementById("startbutton").addEventListener("click", function(){
