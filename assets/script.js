8806042d4a5ce38eb2dc580e2fd609e3a9422719
ericrothmuller <eric@skymarketingco.com>
2022-06-28 00:46:08 -0700

    Revised HTML structure to support quiz questions and gave it styling in CSS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+// Global Variables
+
+// Timer countdown start time is here
+var startTime = 10;
+
+
+
 // Game Questions
 
 var questions = [
@@ -7,7 +14,7 @@ var questions = [
         b: "A technique used to style certain text within the DOM.",
         c: "A special variable which can hold more than one value. It is like a list.",
         d: "A special way to call to the localhost in order to save data.",
-        answer: "C"
+        answer: "C",
     },
     {
         question: "What is global variables?",
@@ -15,7 +22,7 @@ var questions = [
         b: "A variable that has no scope and is available throughout the entire length of code.",
         c: "A variable that is block-scoped and can't be changed through reassignment.",
         d: "It's when you have a function within another function.",
-        answer: "B"
+        answer: "B",
     },
     {
         question: "What is the use of isNaN function?",
@@ -23,38 +30,51 @@ var questions = [
         b: "It always returns no value or no object.",
         c: "Produces a negative infinity number by dividing a negative number by zero.",
         d: "It returns true if the argument is not a number, otherwise it is false.",
-        answer: "D"
+        answer: "D",
     }
 
 
 ]
 
+// Display Quiz
 
-// Button that activates the timer begins here.
+var displayTheQuiz = function displayQuiz() {
+    questionNumber = Math.floor(Math.random() * questions.length);
+    
+        document.getElementById("quizquestion1").innerHTML = questions[questionNumber].question;
+        document.getElementById("quizquestion2").innerHTML;
+        document.getElementById("quizquestion3").innerHTML;
+        document.getElementById("quizquestion4").innerHTML;        
+}
+
+// Button that starts quiz begins here.
 
 document.getElementById("startbutton").addEventListener("click", function(){
-
-// Timer countdown is here
-
-    var startTime = 10;
+    // Timer starts here
+    document.getElementById("timerid").innerHTML = startTime;
 
     var currentTime = setInterval(function() {
         if (startTime > 0){
             var currentTime = --startTime;
             document.getElementById("timerid").innerHTML = startTime;
         }
-
+    
         else {
             document.getElementById("timerid").innerHTML = "You ran out of time!";
         }
     
     }, 1000);
 
-    document.getElementById("timerid").innerHTML = "10";
+    document.getElementById("timerid").innerHTML = startTime;
 
     // Quiz starts here
 
-    document.getElementById("quizdiv").innerHTML = "test";
+    // displayQuiz();
+
+    // currentTime;
+
+    // document.getElementById("quizdiv").innerHTML = "test";
+
+    displayTheQuiz;
 
 });
-
